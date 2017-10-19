3069eaaa6efdd19559f946071d6d0d8707083bec
Luca Tironi <luca.tironi@gmail.com>
2017-10-19 13:30:48 +0200

    Add class to parse and extract metadata from the DESCRIPTION text of the package

---
+++ lib/package_description_parser.rb
@@ -0,0 +1,48 @@
+class PackageDescriptionParser
+  # Adapted from: https://stackoverflow.com/a/11090412
+  # (?<=                Lookbehind assertion.
+  #     ^                   Start at the beginning of the string,
+  #     |                   or,
+  #     [\r\n]              a new line.
+  # )
+  # (                   Capture group 1, the "key".
+  #     [A-Z][^:]*          Capital letter followed as many non-colon
+  #                         characters as possible.
+  # )
+  # :                   The colon character.
+  # \s                  Whitespace
+  # (                   Capture group 2, the "value".
+  #     [^\r\n]*            All characters (i.e. non-newline characters) on the
+  #                         same line belongs to the "value," so take them all.
+
+  #     (?:             Non-capture group.
+
+  #         [\r\n]+         Having already taken everything up to a newline
+  #                         character, take the newline character(s) now.
+
+  #         (?!             Negative lookahead assertion.
+  #             [^A-Z].*:       If this next line contains a capital letter,
+  #                             followed by a string of anything then a colon,
+  #                             then it is a new key/value pair, so we do not
+  #                             want to match this case.
+  #         )
+  #         .*              Providing this isn't the case though, take the line!
+
+  #     )*              And keep taking lines as long as we don't find a
+  #                     key/value pair.
+  # )
+  REGEX = /(?<=^|[\r\n])([A-Z][^:]*):\s([^\r\n]*(?:[\r\n]+(?![A-Z].*:).*)*)/
+
+  def initialize(text)
+    @text = text
+    @data = {}
+  end
+
+  def extract
+    @data = @text.scan(REGEX).reduce({}) do |h, kv_pair|
+      h[kv_pair[0]] = kv_pair[1].gsub(/\n\s+/, ' ').gsub(/\n/, '')
+      h
+    end
+    return @data
+  end
+end
---
+++ spec/package_description_parser_spec.rb
@@ -0,0 +1,50 @@
+require 'spec_helper'
+require 'package_description_parser'
+
+RSpec.describe PackageDescriptionParser do
+  describe '#extract' do
+    let(:package_description_text) {
+<<-EOS
+Package: abc
+Type: Package
+Title: Tools for Approximate Bayesian Computation (ABC)
+Version: 2.1
+Date: 2015-05-04
+Authors@R: c(
+    person("Csillery", "Katalin", role = "aut", email="kati.csillery@gmail.com"),
+    person("Lemaire", "Louisiane", role = "aut"),
+    person("Francois", "Olivier", role = "aut"),
+    person("Blum", "Michael",
+    email = "michael.blum@imag.fr", role = c("aut", "cre")))
+Depends: R (>= 2.10), abc.data, nnet, quantreg, MASS, locfit
+Description: Implements several ABC algorithms for
+        performing parameter estimation, model selection, and goodness-of-fit.
+        Cross-validation tools are also available for measuring the
+        accuracy of ABC estimates, and to calculate the
+        misclassification probabilities of different models.
+Repository: CRAN
+License: GPL (>= 3)
+NeedsCompilation: no
+Packaged: 2015-05-05 08:35:25 UTC; mblum
+Author: Csillery Katalin [aut],
+  Lemaire Louisiane [aut],
+  Francois Olivier [aut],
+  Blum Michael [aut, cre]
+Maintainer: Blum Michael <michael.blum@imag.fr>
+Date/Publication: 2015-05-05 11:34:14
+EOS
+    }
+    let(:parsed_data) { described_class.new(package_description_text).extract }
+
+    it { expect(parsed_data).not_to be_nil }
+    it { expect(parsed_data).to be_kind_of(Hash) }
+    it { expect(parsed_data['Package']).to eq('abc') }
+    it { expect(parsed_data['Version']).to eq('2.1') }
+    it { expect(parsed_data['Title']).to eq('Tools for Approximate Bayesian Computation (ABC)') }
+    it { expect(parsed_data['Date']).to eq('2015-05-04') }
+    it { expect(parsed_data['Date/Publication']).to eq('2015-05-05 11:34:14') }
+    it { expect(parsed_data['Maintainer']).to eq('Blum Michael <michael.blum@imag.fr>') }
+    it { expect(parsed_data['Author']).to eq('Csillery Katalin [aut], Lemaire Louisiane [aut], Francois Olivier [aut], Blum Michael [aut, cre]') }
+    it { expect(parsed_data['Description']).to eq('Implements several ABC algorithms for performing parameter estimation, model selection, and goodness-of-fit. Cross-validation tools are also available for measuring the accuracy of ABC estimates, and to calculate the misclassification probabilities of different models.') }
+  end
+end
