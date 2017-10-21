f2a3095583e167210794f9295025639eb20667af
Luca Tironi <luca.tironi@gmail.com>
2017-10-21 23:37:47 +0200

    Remove wrong parameter for habtm Package's associations

--- app/models/package.rb
+++ app/models/package.rb
@@ -2,7 +2,7 @@
 
 class Package < ApplicationRecord
   has_and_belongs_to_many :authors,
-                          class_name: 'Person', foreign_key: :person_id, join_table: :packages_authors
+                          class_name: 'Person', join_table: :packages_authors
   has_and_belongs_to_many :maintainers,
-                          class_name: 'Person', foreign_key: :person_id, join_table: :packages_maintainers
+                          class_name: 'Person', join_table: :packages_maintainers
 end
