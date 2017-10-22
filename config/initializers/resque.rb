4708c829d44e32a322e4888c90d94cc84cd0e58c
Luca Tironi <luca.tironi@gmail.com>
2017-10-22 15:59:58 +0200

    Make rubocop happy with the latest additions

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -1,2 +1,4 @@
+# frozen_string_literal: true
+
 Resque.redis = "#{ENV.fetch('REDIS_HOST') { 'localhost' }}:#{ENV.fetch('REDIS_PORT') { 6379 }}"
-Resque.after_fork = Proc.new { ActiveRecord::Base.establish_connection }
+Resque.after_fork = proc { ActiveRecord::Base.establish_connection }
