8b5deff9f9e88d1989723bd283cab3ad035a25b8
Luca Tironi <luca.tironi@gmail.com>
2017-10-23 01:10:50 +0200

    Mount the resque-web frontend in the app

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 # frozen_string_literal: true
-
+require 'resque/server'
 Rails.application.routes.draw do
   root 'welcome#index'
 
   resources :packages, only: %i[index show]
 
+  mount Resque::Server, at: '/jobs'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
